02d5dcf33e1452e63832a529669f72ac100868a2
seulmi0827 <dntmfal12@gmail.com>
2025-01-07 11:09:01 +0900

    미완료

---
+++ lambda연습/defaultdict와 lambda.py
@@ -0,0 +1,12 @@
+# defaultdict와 lambda를 활용한 문제 2개 (상세 문제 예시)
+# 기본값이 0인 defaultdict를 활용한 문제
+
+# 문제: 주어진 숫자 리스트에서 각 숫자의 빈도를 계산하세요.
+# 예:
+# python
+# 코드 복사
+# from collections import defaultdict
+# freq_dict = defaultdict(lambda: 0)
+# nums = [1, 2, 2, 3, 3, 3]
+# # 여기에 nums 리스트의 빈도를 계산하도록 코드를 작성하세요.
+# print(freq_dict)  # 출력: {1: 1, 2: 2, 3: 3}
