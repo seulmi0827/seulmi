e2c663dcc1cb23e43ac6a1e1e569e8085ba6f89e
seulmi0827 <dntmfal12@gmail.com>
2025-01-20 23:24:08 +0900

    Delete Python/백준/Bronze directory

--- Python/백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-result = []
-for _ in range(n):
-    i = input()
-    result.append(i)
-start = result[0]
-for i in result[1:]:
-    start = ''.join([j if j == start[idx] else '?' for idx, j in enumerate(i)])
-print(start)
