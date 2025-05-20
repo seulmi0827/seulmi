ada93c61b0ac82adb08000bdb1c7c737bcf9051d
seulmi0827 <dntmfal12@gmail.com>
2025-05-20 23:44:27 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -0,0 +1,18 @@
+-- 코드를 입력하세요
+SELECT 
+    case 
+    when price <10000 then '0'
+    when price >=10000 and price <20000 then '10000'
+    when price >=20000 and price <30000 then '20000'
+    when price >=30000 and price <40000 then '30000'
+    when price >=40000 and price <50000 then '40000'
+    when price >=50000 and price <60000 then '50000'
+    when price >=60000 and price <70000 then '60000'
+    when price >=70000 and price <80000 then '70000'
+    when price >=80000 and price <90000 then '80000'
+    end as price_group
+    ,count(product_id) as products
+from product
+group by price_group
+order by price_group
+;
