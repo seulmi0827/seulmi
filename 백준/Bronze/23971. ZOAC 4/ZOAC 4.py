a758c986b2e90637c54d03eee00831511a611d9a
seulmi0827 <dntmfal12@gmail.com>
2025-06-24 09:59:50 +0900

    폴더정리

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
+++
@@ -1,4 +0,0 @@
-H,W,h,w = map(int,input().split())
-h_re = int(((H-1) - (H-1)%(h+1))/(h+1))
-w_re = int(((W-1) - (W-1)%(w+1))/(w+1))
-print((h_re+1)*(w_re+1))
--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++
@@ -1,8 +0,0 @@
-h, m = map(int,input().split())
-m_re = m - 45
-if m_re < 0:
-    m_re += 60
-    h -= 1
-    if h <0:
-        h +=24
-print(h,m_re)
