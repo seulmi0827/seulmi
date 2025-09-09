81cb1343d7ff18801358ed62d0c6090c5c1c64f9
seulmi0827 <dntmfal12@gmail.com>
2025-09-09 12:10:49 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ MySQL/프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,14 +1,13 @@
 -- 코드를 입력하세요
-select title, board_id, reply_id, writer_id, contents, date_format(created_date,'%Y-%m-%d') from 
-(SELECT 
+SELECT 
     board.title,
     board.board_id,  
     reply.reply_id, 
     reply.writer_id, 
     reply.contents, 
-    reply.created_date
+    date_format(reply.created_date,'%Y-%m-%d') as created_date
 from used_goods_board as board
 join used_goods_reply as reply
 on board.board_id =  reply.board_id
 where board.created_date like '2022-10%'
-order by created_date, title) as m
+order by created_date, title
